22fd867fd04dd28b97193b4b6e12a13f88f14b37
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-06-13 20:04:33 +0100

    ?

---
+++ ecom/src/App.js
@@ -0,0 +1,17 @@
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Home from "../src/pages/Home"
+
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;

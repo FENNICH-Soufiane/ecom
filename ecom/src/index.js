d1ca3c33a905b8351caedfd8e77a8264d77bf2dc
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-06-17 13:40:08 +0100

    simple manipulation

---
+++ ecom/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ // Remplacez par votre clé publique Stripe
+
+ReactDOM.render(
+  <React.StrictMode>
+    
+      <App />
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
